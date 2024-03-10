d96ca9257f97b2e910b531f420e759666179c471
genta <genta@gmail>
2024-03-11 00:20:07 +0900

    5th commit

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT =BASE_DIR/'static'
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR/'media/'
-MEDIA_URL = 'edia/'
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
